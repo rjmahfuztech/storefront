b722131d01e8eac57e13896eeb1e87fed725e73a
rjmahfuztech <rjmahfuztech@gmail.com>
2025-09-13 19:19:24 +0600

    store and tags app created

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "debug_toolbar",
-    'playground'
+    'playground',
+    'store',
+    'tags',
 ]
 
 MIDDLEWARE = [
