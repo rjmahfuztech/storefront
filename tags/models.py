0ae944b747a2bc4ac11c1c2839d3112455e98722
rjmahfuztech <rjmahfuztech@gmail.com>
2025-09-16 22:42:17 +0600

    Tag and TagItem class added

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    # What tag applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # Type (Product, Video, Article ...etc)
+    # ID
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
