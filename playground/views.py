7ab33aa29c072010224e2d60fb670c28aa699a9c
rjmahfuztech <rjmahfuztech@gmail.com>
2025-09-23 23:51:38 +0600

    products load from database and show on web

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 
 
 def say_hello(request):
-    x = 1
-    y = 2
-    return render(request, 'hello.html', {'name': 'Mahfuz Islam'})
+    products = Product.objects.all()
+
+    return render(request, 'hello.html', {'name': 'Mahfuz Islam', 'products': products})
