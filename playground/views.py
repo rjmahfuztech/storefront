12d6662805a5ee9c52fd0366bbbd7d148ef37d68
rjmahfuztech <rjmahfuztech@gmail.com>
2025-09-24 19:04:25 +0600

    filter product with complex query using and, or, not with Q operator

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 from store.models import Product
 
 
 
 def say_hello(request):
-    products = Product.objects.all()
+    # Complex lookups Using Q objects
 
-    return render(request, 'hello.html', {'name': 'Mahfuz Islam', 'products': products})
+    # Products: inventory < 20 AND price < 500
+    # products = Product.objects.filter(inventory__lt=20, unit_price__lt=500)
+    # another way for same filter
+    # products = Product.objects.filter(inventory__lt=20).filter(unit_price__lt=500)
+
+    # Products: inventory < 20 OR price < 500
+    # products = Product.objects.filter(Q(inventory__lt=20) | Q(unit_price__lt=500))
+
+    # Products: inventory < 20 & NOT (~) price < 500
+    products = Product.objects.filter(Q(inventory__lt=20) | ~Q(unit_price__lt=500))
+
+
+
+    return render(request, 'hello.html', {'name': 'Mahfuz Islam', 'products': list(products)})
