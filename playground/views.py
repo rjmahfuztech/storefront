f5da44e1ba20d3acd532d31825d433c843dd1ecc
rjmahfuztech <rjmahfuztech@gmail.com>
2025-09-26 22:33:08 +0600

    selecting related object using select_related and prefetch_related

--- playground/views.py
+++ playground/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
-from store.models import Product, OrderItem
+from store.models import Product, Order, OrderItem
 
 
 
 def say_hello(request):
-    # Deferring Fields
-    queryset = Product.objects.only('id', 'title') # load only id and title from DB
-    queryset = Product.objects.defer('description') # load all data but not description
+    # Selecting related object
+    queryset = Product.objects.select_related('collection').all() # works for foreignkey / oneToOneField
+    queryset = Product.objects.prefetch_related('promotion').all() # works for manyToManyField / reverse foreignkey
 
-    # so only() load the data we want and later if we want other field then it will query again for load that data
-    # and defer() do not load the data that we select and if we need it later then it will query again for load that data
-    
+    # practice (get the last 5 orders with their customer and items (including product))
+    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-id')[:5]
 
 
-    return render(request, 'hello.html', {'name': 'Mahfuz Islam', 'products': list(queryset)})
+
+
+    return render(request, 'hello.html', {'name': 'Mahfuz Islam', 'orders': list(queryset)})
