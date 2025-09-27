5922d340494159072c6191a43f9201907ba11509
rjmahfuztech <rjmahfuztech@gmail.com>
2025-09-27 23:28:50 +0600

    practice aggregating object

--- playground/views.py
+++ playground/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.db.models import Q, F
-from store.models import Product, Order, OrderItem
+from django.db.models.aggregates import Avg, Max, Min, Count
+from store.models import Product
 
 
 
 def say_hello(request):
-    # Selecting related object
-    queryset = Product.objects.select_related('collection').all() # works for foreignkey / oneToOneField
-    queryset = Product.objects.prefetch_related('promotion').all() # works for manyToManyField / reverse foreignkey
-
-    # practice (get the last 5 orders with their customer and items (including product))
-    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-id')[:5]
+    # Aggregating objects
+    result = Product.objects.filter(collection__id=1).aggregate(count=Count('id'), min_price=Min('unit_price'))
 
 
-
-
-    return render(request, 'hello.html', {'name': 'Mahfuz Islam', 'orders': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Mahfuz Islam', 'result': result})
