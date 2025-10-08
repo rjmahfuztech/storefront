25d704b89241d921bfdfa212bb7f1ce154ff514a
rjmahfuztech <rjmahfuztech@gmail.com>
2025-10-08 23:28:40 +0600

    practice updating and deleting objects

--- playground/views.py
+++ playground/views.py
@@ -4,16 +4,25 @@ from store.models import Product, Collection
 
 
 def say_hello(request):
-    # Creating Objects
-    collection = Collection(title='video games', featured_product_id=1)
-
-    # another way
-    collection = Collection()
+    # updating objects
+    collection = Collection.objects.get(pk=11)
     collection.title = 'Video Games'
-    collection.featured_product = Product(pk=1)
+    collection.featured_product = None
     collection.save()
 
-    # another way using create method
-    Collection.objects.create(title='Video Games', featured_product_id=1)
+    # another way
+    Collection.objects.filter(pk=11).update(featured_product=None)
+
+
+    # deleting objects
+    
+    # single delete
+    collection = Collection(pk=13)
+    # collection.delete()
+
+    # multiple delete
+    Collection.objects.filter(id__gt=11).delete()
+
+
 
     return render(request, 'hello.html', {'name': 'Mahfuz Islam',})
