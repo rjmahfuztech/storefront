98afb554128b96d92f92544817b48eccc2f01723
rjmahfuztech <rjmahfuztech@gmail.com>
2025-10-13 12:32:43 +0600

    Adding search to the customer list page

--- store/admin.py
+++ store/admin.py
@@ -40,6 +40,8 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
     def orders_count(self, customer):
