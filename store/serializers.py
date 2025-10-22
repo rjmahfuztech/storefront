39663fbd58697f9a57027b4b04dbf684ce389498
rjmahfuztech <rjmahfuztech@gmail.com>
2025-10-22 12:21:43 +0600

    added custom field price_with_tax in serializers and serializing relationship for collection in 4 ways for practice

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
+from .models import Product, Collection
+from decimal import Decimal
 
+# Creating custom serializer fields
+# Serializing relationships
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=260)
-    unit_price = serializers.DecimalField(max_digits=12, decimal_places=2)
+    price = serializers.DecimalField(max_digits=12, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
+    # collection = serializers.StringRelatedField() # another way to show the string of the collection
+    # collection = CollectionSerializer() # another way to add nested object of the collection
+    collection = serializers.HyperlinkedRelatedField(queryset=Collection.objects.all(), view_name='collection-detail')
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
