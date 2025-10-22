21c200499d925f5d7f43687c3bfa5cb2828ccfa3
rjmahfuztech <rjmahfuztech@gmail.com>
2025-10-22 19:03:22 +0600

    related name added to Orderitem and update and delete method added to product_detail

--- store/views.py
+++ store/views.py
@@ -7,7 +7,7 @@ from .serializers import ProductSerializer
 from .models import Product
 
 
-# Deserializing Objects and data validation
+
 @api_view(['GET', 'POST'])
 def product_list(request):
     if request.method == 'GET':
@@ -17,16 +17,27 @@ def product_list(request):
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        print(serializer.validated_data)
-        return Response('Ok')
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        if product.orderitems.count() > 0:
+            return Response({'error': 'Product can not be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
