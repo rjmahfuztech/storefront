f2c9c814f907da41da4600a7a7135e62adba8bf2
rjmahfuztech <rjmahfuztech@gmail.com>
2025-10-31 21:15:03 +0600

    create custom pagination class and added it to the ProductViewSet

--- store/views.py
+++ store/views.py
@@ -7,6 +7,7 @@ from django.db.models import Count
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from .models import Product, Collection, OrderItem, Review
 from .filters import ProductFilter
+from .pagination import DefaultPagination
 
 
 
@@ -15,6 +16,7 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     search_fields = ['title', 'description']
     ordering_fields = ['unit_price', 'last_update']
 
