fac8e6267d2c1f0a5ddf14a2db5b884e3c8576da
rjmahfuztech <rjmahfuztech@gmail.com>
2025-11-04 19:47:12 +0600

    create custom serializers and add more fields in default djoser serializers and connect it in settings

--- storefront/settings.py
+++ storefront/settings.py
@@ -153,3 +153,10 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
+
+
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'core.serializers.UserCreateSerializer',
+    },
+}
